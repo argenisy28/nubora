405247cba73e7174c3521cafdc10ea8e41eae5dd
Argenis Y. Velez Alvarez <argenisy28@gmail.com>
2026-09-17 01:53:13 -0400

    Add Nubora health check Lambda

---
+++ backend/functions/health/lambda_function.py
@@ -0,0 +1,14 @@
+import json
+
+
+def lambda_handler(event, context):
+    return {
+        "statusCode": 200,
+        "headers": {
+            "content-type": "application/json"
+        },
+        "body": json.dumps({
+            "status": "healthy",
+            "service": "Nubora API"
+        })
+    }
